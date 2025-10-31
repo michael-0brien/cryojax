49944cd5b5fae0edf3ed394339847ad88d1e6542
Geoffrey Woollard <geoffwoollard@gmail.com>
2025-10-31 01:29:23 -0400

    doc string and clean up comments

--- cryojax/simulator/_volume_integrator/fft_delta_atom_projection.py
+++ cryojax/simulator/_volume_integrator/fft_delta_atom_projection.py
@@ -1,5 +1,6 @@
 from functools import partial
 from typing import ClassVar
+from typing_extensions import override
 
 import jax
 import jax.numpy as jnp
@@ -18,7 +19,8 @@ class FFTDeltaAtomProjection(
     strict=True,
 ):
     """Integrate atomic parametrization of a volume onto
-    the exit plane using non-uniform FFTs plus convolution.
+    the exit plane using integer rounded coordinates and
+    jax.scipy.signal.fftconvolve for Gaussian convolution.
     """
 
     n_kernel_truncation_pix: int
@@ -38,26 +40,15 @@ class FFTDeltaAtomProjection(
     ):
         """**Arguments:**
 
-        - `antialias`:
-            If `True`, apply an anti-aliasing filter to more accurately
-            sample the volume.
+        - `n_kernel_truncation_pix`:
+            The size of the Gaussian kernel (in pixels) used for convolution.
+        - `kernel_width_pix`:
+            The width (standard deviation) of the Gaussian kernel (in pixels).
         - `upsample_factor`:
-            If provided, first compute an upsampled version of the
-            image at pixel size `image_config.pixel_size / upsample_factor`.
-            Then, downsample with `cryojax.ndimage.block_reduce_downsample`
-            to locally average to the correct pixel size. This is useful
-            for reducing aliasing.
+            If not `None`, the factor by which to upsample the image
+            to reduce aliasing effects.
         - `shape`:
-            If given, first compute the image at `shape`, then
-            pad or crop to `image_config.padded_shape`.
-        - `eps`:
-            See [`jax-finufft`](https://github.com/flatironinstitute/jax-finufft)
-            for documentation.
-        - `opts`:
-            A `jax_finufft.options.Opts` or `jax_finufft.options.NestedOpts`
-            dataclass.
-            See [`jax-finufft`](https://github.com/flatironinstitute/jax-finufft)
-            for documentation.
+            If not `None`, the shape of the output projection images.
         """
 
         self.upsample_factor = upsample_factor
@@ -74,6 +65,7 @@ class FFTDeltaAtomProjection(
                     "`FFTAtomProjection`, but only odd `upsample_factor` are supported."
                 )
 
+    @override
     def integrate(
         self,
         atom_volume: IndependentAtomVolume,
@@ -105,96 +97,6 @@ class FFTDeltaAtomProjection(
             return real_space_proj
         else:
             return rfftn(real_space_proj.reshape(n_pix, n_pix))
-
-    # @override
-    # def integrate(
-    #     self,
-    #     volume_representation: IndependentAtomVolume,
-    #     image_config: AbstractImageConfig,
-    #     outputs_real_space: bool = False,
-    # ) -> (
-    #     Complex[
-    #         Array,
-    #         "{image_config.padded_y_dim} {image_config.padded_x_dim//2+1}",
-    #     ]
-    #     | Float[Array, "{image_config.padded_y_dim} {image_config.padded_x_dim}"]
-    # ):
-    #     """Compute a projection from scattering factors per atom type
-    #     from the `IndependentAtomVolume`.
-
-    #     **Arguments:**
-
-    #     - `volume_representation`:
-    #         The volume representation.
-    #     - `image_config`:
-    #         The configuration of the resulting image.
-    #     - `outputs_real_space`:
-    #         If `True`, return the image in real space. Otherwise,
-    #         return in fourier.
-
-    #     **Returns:**
-
-    #     The integrated volume in real or fourier space at the
-    #     `AbstractImageConfig.padded_shape`.
-    #     """  # noqa: E501
-    #     u = self.upsample_factor
-    #     pixel_size = image_config.pixel_size
-    #     shape = image_config.padded_shape if self.shape is None else self.shape
-    #     if u is None:
-    #         shape_u, pixel_size_u = shape, pixel_size
-    #     else:
-    #         shape_u, pixel_size_u = (u * shape[0], u * shape[1]), pixel_size / u
-    #     if shape_u == image_config.padded_shape:
-    #         frequency_grid = image_config.padded_full_frequency_grid_in_angstroms
-    #     else:
-    #         frequency_grid = make_frequency_grid(
-    #             shape_u, pixel_size_u, outputs_rfftfreqs=False
-    #         )
-    #     frequency_grid = jnp.fft.fftshift(frequency_grid, axes=(0, 1))
-    #     proj_kernel = lambda pos, kernel: _project_with_nufft(
-    #         shape_u,
-    #         pixel_size_u,
-    #         pos,
-    #         kernel,
-    #         frequency_grid,
-    #         eps=self.eps,
-    #         opts=self.opts,
-    #     )
-    #     # Compute projection over atom types
-    #     fourier_projection = jax.tree.reduce(
-    #         lambda x, y: x + y,
-    #         jax.tree.map(
-    #             proj_kernel,
-    #             volume_representation.position_pytree,
-    #             volume_representation.scattering_factor_pytree,
-    #             is_leaf=lambda x: isinstance(x, op.AbstractFourierOperator),
-    #         ),
-    #     )
-    #     # Apply anti-aliasing filter
-    #     if self.antialias:
-    #         antialias_fn = op.FourierSinc(box_width=pixel_size_u)
-    #         fourier_projection *= antialias_fn(frequency_grid)
-    #     # Shift zero frequency component to corner and convert to
-    #     # rfft
-    #     fourier_projection = convert_fftn_to_rfftn(
-    #         jnp.fft.ifftshift(fourier_projection), mode="real"
-    #     )
-    #     if self.shape is None:
-    #         if u is None:
-    #             return (
-    #                 irfftn(fourier_projection, s=shape)
-    #                 if outputs_real_space
-    #                 else fourier_projection
-    #             )
-    #         else:
-    #             projection = _block_average(irfftn(fourier_projection, s=shape_u), u)
-    #             return projection if outputs_real_space else rfftn(projection)
-    #     else:
-    #         projection = irfftn(fourier_projection, s=shape_u)
-    #         if u is not None:
-    #             projection = _block_average(projection, u)
-    #         projection = resize_with_crop_or_pad(projection, image_config.padded_shape)
-    #         return projection if outputs_real_space else rfftn(projection)
 
 
 def precompute_gaussian_kernel(n_trunc, sigma):
