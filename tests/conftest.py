6ca36cf6704331b94e3671ca6db12e4d53277c27
Michael O'Brien <michaelobrien@g.harvard.edu>
2026-06-17 19:30:00 +0200

    simplify IndependentAtomVolume class to gaussian case

--- tests/conftest.py
+++ tests/conftest.py
@@ -43,8 +43,3 @@ def sample_waterbox_pdb():
 @pytest.fixture
 def peng_parameters_path():
     return os.path.join(os.path.dirname(__file__), "data", "peng1996_params.npy")
-
-
-@pytest.fixture
-def lobato_parameters_path():
-    return os.path.join(os.path.dirname(__file__), "data", "lobato2014_params.npy")
